3a4f627f845bf5983cd41b78bb8766c7a8bb1ef2
cperafan <cperafan@mercadolibre.com.co>
2022-10-13 19:44:44 -0500

    add register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { JwtModule } from '@auth0/angular-jwt';
+
+export const LOCALSTORAGE_TOKEN_KEY = 'sport_app_token';
+
+export function tokenGetter() {
+  return localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:3000', 'localhost:8080']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
