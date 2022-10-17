877aa7b7e8a5dba6d714bdc32d71c948ecbf971d
Joan Blanco <78229149+jblancov7764@users.noreply.github.com>
2022-10-16 19:23:22 -0500

    plantilla para paginas y modulo plans agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import localeEs from '@angular/common/locales/es-CO'
+import { registerLocaleData } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlanComponent } from './plan/plan.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(localeEs, 'es');
+
+const routes: Routes = [
+  {path: '', redirectTo: 'planes', pathMatch: 'full'},
+  {path: 'planes', component: PlanComponent},
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PlanComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
